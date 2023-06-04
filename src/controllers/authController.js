c433b406c7fdcd721cb97d9454a218d741f99fa8
Raphael <raphael.girao@unimedfortaleza.com.br>
2023-06-04 17:50:40 -0300

    Iniciando o projeto

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,32 +1,15 @@
-import bcryptjs from 'bcryptjs';
-import jsonwebtoken from 'jsonwebtoken';
-import Usuario from '../models/usuario.js';
-import constants from '../config/constants.js';
+import AuthService from '../services/authService.js';
+
+const authService = new AuthService();
 
 class AuthController {
   static login = async (req, res) => {
     const { body } = req;
+
     try {
-      const usuario = await Usuario.pegarPeloEmail(body.email);
+      const login = await authService.login(body);
 
-      if (!usuario) {
-        throw new Error('Usuario não cadastrado');
-      }
-
-      const senhaIguais = await bcryptjs.compare(body.password, usuario.password);
-
-      if (!senhaIguais) {
-        throw new Error('Usuario ou senha invalido');
-      }
-
-      const accessToken = jsonwebtoken.sign({
-        id: usuario.id,
-        email: usuario.email,
-      }, constants.jsonSecret, {
-        expiresIn: 86400,
-      });
-
-      return res.status(201).json({ message: 'Usuario conectado', accessToken });
+      return res.status(201).json(login);
     } catch (err) {
       return res.status(500).json(err.message);
     }
@@ -34,11 +17,11 @@ class AuthController {
 
   static cadastrarUsuario = async (req, res) => {
     const { body } = req;
-    body.password = await bcryptjs.hash(body.password, 8);
-    const usuario = new Usuario(body);
+    
     try {
-      const resposta = await usuario.salvar(usuario);
-      return res.status(201).json({ message: 'usuario criado', content: resposta });
+      const usuario = await authService.cadastrarUsuario(body);
+
+      return res.status(201).json(usuario);
     } catch (err) {
       return res.status(500).json(err.message);
     }
---
+++ src/services/authService.js
@@ -0,0 +1,47 @@
+import bcryptjs from 'bcryptjs';
+import jsonwebtoken from 'jsonwebtoken';
+import Usuario from '../models/usuario.js';
+import constants from '../config/constants.js';
+
+class AuthService {
+  async login(data) {
+    try {
+      const usuario = await Usuario.pegarPeloEmail(data.email);
+
+      if (!usuario) {
+        throw new Error('Usuario não cadastrado');
+      }
+
+      const senhaIguais = await bcryptjs.compare(data.password, usuario.password);
+
+      if (!senhaIguais) {
+        throw new Error('Usuario ou senha invalido');
+      }
+
+      const accessToken = jsonwebtoken.sign({
+        id: usuario.id,
+        email: usuario.email,
+      }, constants.jsonSecret, {
+        expiresIn: 86400,
+      });
+
+      return { message: 'Usuario conectado', accessToken };
+    } catch (err) {
+      return err.message;
+    }
+  }
+
+  async cadastrarUsuario(data) {
+    data.password = await bcryptjs.hash(data.password, 8);
+    
+    const usuario = new Usuario(data);
+    try {
+      const resposta = await usuario.salvar(usuario);
+      return { message: 'usuario criado', content: resposta };
+    } catch (err) {
+      return err.message;
+    }
+  }
+}
+
+export default AuthService;
